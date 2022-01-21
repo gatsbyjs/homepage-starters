5e062700aabd0e254867fd7d7ba44813797e127b
Brent Jackson <jxnblk@gmail.com>
2022-01-21 16:39:04 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     // Comment and uncomment the desired CMS/backend plugin to switch between data sources
     // 'yaml-homepage-plugin',
     // 'datocms-homepage-plugin',
-    'contentful-homepage-plugin',
+    "contentful-homepage-plugin",
     // 'wordpress-homepage-plugin',
   ],
 }
