35a4fc05d5af46720ba05c20be6bf818c72706ae
jxnblk <jxnblk@gmail.com>
2022-01-31 14:56:23 -0500

    Switch to Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     // Comment and uncomment the desired CMS/backend plugin to switch between data sources
     // "yaml-plugin",
     // "datocms-plugin",
-    // "contentful-plugin",
-    "wordpress-plugin",
+    "contentful-plugin",
+    // "wordpress-plugin",
   ],
 }
