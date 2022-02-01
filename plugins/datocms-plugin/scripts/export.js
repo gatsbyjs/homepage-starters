297a9da5a97c7ee82ebad303efe56b040b4ac84d
jxnblk <jxnblk@gmail.com>
2022-02-01 10:10:57 -0500

    Add DatoCMS export script

---
+++ plugins/datocms-plugin/scripts/export.js
@@ -0,0 +1,45 @@
+require("dotenv").config({
+  path: ".env.development",
+})
+const SiteClient = require("datocms-client").SiteClient
+const fs = require("fs")
+const path = require("path")
+// const request = require('request')
+const axios = require("axios")
+const client = new SiteClient(process.env.DATOCMS_API_TOKEN)
+
+console.log("Downloading DatoCMS records...")
+
+client.items
+  .all({}, { allPages: true })
+  .then((response) => {
+    fs.writeFileSync("scripts/data.json", JSON.stringify(response, null, 2))
+  })
+  .then(() => {
+    return client.site.find()
+  })
+  .then((site) => {
+    client.uploads.all({}, { allPages: true }).then((uploads) => {
+      return uploads.reduce((chain, upload) => {
+        return chain.then(() => {
+          return new Promise((resolve) => {
+            const imageUrl = "https://" + site.imgixHost + upload.path
+            console.log(`Downloading ${imageUrl}...`)
+            const stream = fs.createWriteStream(
+              path.join(".", "scripts", "assets", path.basename(upload.path))
+            )
+            axios({
+              method: "get",
+              responseType: "stream",
+              url: imageUrl,
+            }).then((res) => {
+              res.data.pipe(stream)
+            })
+            stream.on("close", resolve)
+          })
+        })
+      }, Promise.resolve())
+    })
+  })
+
+console.log("Got records")
