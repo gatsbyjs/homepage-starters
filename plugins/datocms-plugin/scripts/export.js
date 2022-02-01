07a8085c346056f5b8a3e960ced3527a486f6e09
jxnblk <jxnblk@gmail.com>
2022-02-01 16:34:24 -0500

    Attempting to import data back into datocms

--- plugins/datocms-plugin/scripts/export.js
+++ plugins/datocms-plugin/scripts/export.js
@@ -9,37 +9,79 @@ const client = new SiteClient(process.env.DATOCMS_API_TOKEN)
 
 console.log("Downloading DatoCMS records...")
 
-// https://www.datocms.com/docs/import-and-export/export-data
-client.items
-  .all({}, { allPages: true })
-  .then((response) => {
-    fs.writeFileSync("scripts/data.json", JSON.stringify(response, null, 2))
-  })
-  .then(() => {
-    return client.site.find()
-  })
-  .then((site) => {
-    client.uploads.all({}, { allPages: true }).then((uploads) => {
-      return uploads.reduce((chain, upload) => {
-        return chain.then(() => {
-          return new Promise((resolve) => {
-            const imageUrl = "https://" + site.imgixHost + upload.path
-            console.log(`Downloading ${imageUrl}...`)
-            const stream = fs.createWriteStream(
-              path.join(".", "scripts", "assets", path.basename(upload.path))
-            )
-            axios({
-              method: "get",
-              responseType: "stream",
-              url: imageUrl,
-            }).then((res) => {
-              res.data.pipe(stream)
-            })
-            stream.on("close", resolve)
-          })
-        })
-      }, Promise.resolve())
-    })
-  })
+const data = {}
 
-console.log("Got records")
+// https://www.datocms.com/docs/import-and-export/export-data
+async function exportContent() {
+  data.items = await client.items.all({}, { allPages: true })
+
+  const site = await client.site.find()
+  const uploads = await client.uploads.all({}, { allPages: true })
+  await uploads.reduce((chain, upload) => {
+    return chain.then(() => {
+      return new Promise((resolve) => {
+        const imageUrl = "https://" + site.imgixHost + upload.path
+        console.log(`Downloading ${imageUrl}...`)
+        const stream = fs.createWriteStream(
+          path.join(".", "scripts", "assets", path.basename(upload.path))
+        )
+        axios({
+          method: "get",
+          responseType: "stream",
+          url: imageUrl,
+        }).then((res) => {
+          res.data.pipe(stream)
+        })
+        stream.on("close", resolve)
+      })
+    })
+  }, Promise.resolve())
+
+  /*
+    .then(() => {
+      return client.site.find()
+    })
+    .then((site) => {
+      return client.uploads.all({}, { allPages: true }).then((uploads) => {
+        return uploads.reduce((chain, upload) => {
+          return chain.then(() => {
+            return new Promise((resolve) => {
+              const imageUrl = "https://" + site.imgixHost + upload.path
+              console.log(`Downloading ${imageUrl}...`)
+              const stream = fs.createWriteStream(
+                path.join(".", "scripts", "assets", path.basename(upload.path))
+              )
+              axios({
+                method: "get",
+                responseType: "stream",
+                url: imageUrl,
+              }).then((res) => {
+                res.data.pipe(stream)
+              })
+              stream.on("close", resolve)
+            })
+          })
+        }, Promise.resolve())
+      })
+    })
+  */
+
+  data.itemTypes = await client.itemTypes.all()
+
+  data.fields = []
+
+  for (let i = 0; i < data.itemTypes.length; i++) {
+    const itemType = data.itemTypes[i]
+    for (let j = 0; j < itemType.fields.length; j++) {
+      const fieldID = itemType.fields[j]
+      const field = await client.fields.find(fieldID)
+      data.fields.push(field)
+    }
+  }
+
+  const json = JSON.stringify(data, null, 2)
+  fs.writeFileSync("scripts/data.json", json)
+  console.log("Got all items and itemTypes")
+}
+
+exportContent(process.env.DATOCMS_API_TOKEN)
--- plugins/datocms-plugin/scripts/import.js
+++ plugins/datocms-plugin/scripts/import.js
@@ -1,14 +1,119 @@
 // https://www.datocms.com/docs/import-and-export/importing-data#step-3-importing-to-datocms
+const fs = require("fs")
 const uniq = require("lodash.uniq")
 const { SiteClient } = require("datocms-client")
 const data = require("./data.json")
 
+// fields that link to other fields must be created
+// after the linked field
+// The API does not sort itemTypes based on dependencies
+// so this list ensures itemTypes and fields are created
+// in the correct order
+const orderedItemTypes = [
+  "Link",
+  "Social Link",
+  "Benefit",
+  "Feature",
+  "Product",
+  "Stat",
+  "Testimonial",
+  "Hero",
+  "CTA",
+  "BenefitList",
+  "FeatureList",
+  "LogoList",
+  "ProductList",
+  "StatList",
+  "TestimonialList",
+  "Homepage",
+  "LayoutHeader",
+  "LayoutFooter",
+  "Layout",
+  "Page",
+]
+
 async function importContent(token) {
   const client = new SiteClient(token)
-  for (let i = 0; i < data.length; i++) {
-    const { id, meta, creator, updatedAt, createdAt, ...item } = data[i]
-    const record = await client.item.create(item)
-    console.log("created", item.id)
+  const { itemTypes, fields, items } = data
+
+  const nextItemTypes = []
+  const errors = []
+
+  // map itemType ids to names
+  items.forEach((item) => {
+    item.itemType = itemTypes.find((t) => t.id === item.itemType)?.name
+    if (!item.itemType) {
+      throw new Error(`Missing itemType: ${item}`)
+    }
+  })
+
+  for (let i = 0; i < orderedItemTypes.length; i++) {
+    const name = orderedItemTypes[i]
+    const type = itemTypes.find((t) => t.name === name)
+    if (!type) {
+      console.error(`No item type found: ${name}`)
+      break
+    }
+    // prettier-ignore
+    const {
+      id,
+      // Type creation can fail when titleField is defined
+      titleField,
+      ...itemType
+    } = type
+
+    try {
+      const record = await client.itemType.create(itemType)
+      nextItemTypes.push(record)
+      for (let j = 0; j < itemType.fields.length; j++) {
+        const field = fields.find((f) => f.id === itemType.fields[j])
+        try {
+          // some fields can reference other itemTypes
+          if (field.id) {
+            const { id, ...attr } = field
+            const nextField = await client.field.create(record.id, attr)
+          }
+        } catch (e) {
+          const existingNextField = await client.field.find(field.apiKey)
+          if (existingNextField.id) {
+            console.log(`${field.label} already created`)
+          } else {
+            console.error(`Error creating field ${field.label}`)
+            errors.push(e)
+            fs.writeFileSync(
+              "datocms-errors.log",
+              JSON.stringify(errors, null, 2)
+            )
+          }
+        }
+      }
+      console.log("created Item Type", record.name)
+    } catch (e) {
+      console.error(`Error creating Item Type: ${itemType.name} - ${e.message}`)
+      errors.push(e)
+      // throw new Error(e)
+      // TODO handle existing item Types
+      fs.writeFileSync("datocms-errors.log", JSON.stringify(errors, null, 2))
+    }
+  }
+
+  console.log("Importing content")
+  fs.writeFileSync(
+    "temp-items.json",
+    JSON.stringify({ items, __itemTypes: nextItemTypes }, null, 2)
+  )
+
+  for (let i = 0; i < items.length; i++) {
+    const { id, meta, creator, updatedAt, createdAt, ...item } = items[i]
+    // remap new itemType ids based on name
+    item.itemType = nextItemTypes.find((t) => t.name === item.itemType).id
+    try {
+      const record = await client.item.create(item)
+    } catch (e) {
+      console.error(`Error creating item: ${id} - ${e.message}`)
+      errors.push(e)
+      fs.writeFileSync("datocms-errors.log", JSON.stringify(errors, null, 2))
+    }
   }
 
   /* TODO
@@ -24,9 +129,15 @@ async function importContent(token) {
       }
     )
     */
+
+  if (errors.length) {
+    fs.writeFileSync("datocms-errors.log", JSON.stringify(errors, null, 2))
+  }
 }
 
-console.log(`Importing ${data.length} DatoCMS records`)
+console.log(
+  `Importing ${data.items.length} DatoCMS Items and ${data.itemTypes.length} Item Types`
+)
 
 importContent(process.env.DATOCMS_API_TOKEN)
 
