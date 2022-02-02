04036fe88773cfa91b1a573c5fc0b612cc3cfb4c
Leithon English <leithonenglish@gmail.com>
2022-02-02 08:47:32 -0500

    added drupal specific changes to config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     // Comment and uncomment the desired CMS/backend plugin to switch between data sources
     // "yaml-plugin",
     // "datocms-plugin",
+    // "drupal-plugin",
     "contentful-plugin",
     // "wordpress-plugin",
   ],
