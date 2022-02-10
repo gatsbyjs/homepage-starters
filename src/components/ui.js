51e9a2904e84575c4aef98ba49cc863094a168e9
Leithon English <leithonenglish@gmail.com>
2022-02-09 19:55:15 -0500

    minor ui update

--- src/components/ui.js
+++ src/components/ui.js
@@ -101,7 +101,7 @@ export function Nudge({ left, right, top, bottom, ...props }) {
 }
 
 export function Section(props) {
-  return <Box as="section" paddingY={5} {...props} />
+  return <Box as="section" paddingY={7} {...props} />
 }
 
 export function Text({ variant = "body", center, bold, ...props }) {
