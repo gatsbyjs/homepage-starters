5535e2ef421dc5ec5f99cb88b7d93409eab71d59
Leithon English <leithonenglish@gmail.com>
2022-02-14 12:24:00 -0500

    another fake commit

--- plugins/drupal-plugin/gatsby-node.js
+++ plugins/drupal-plugin/gatsby-node.js
@@ -264,7 +264,7 @@ exports.createSchemaCustomization = async ({ actions }) => {
     }
   `)
 
-  // CMS-specific type
+  // CMS-specific types
   actions.createTypes(/* GraphQL */ `
     type node__homepage_link implements Node & HomepageLink @dontInfer {
       id: ID!
