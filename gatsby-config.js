9ba3c2a2875445a759ec16519efee381c4021f7f
jxnblk <jxnblk@gmail.com>
2022-02-15 16:12:00 -0500

    Rename nav item types for drupal

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   plugins: [
     // Comment and uncomment the desired CMS/backend plugin to switch between data sources
     // "contentful-plugin",
-    "datocms-plugin",
-    // "drupal-plugin",
+    // "datocms-plugin",
+    "drupal-plugin",
     // "wordpress-plugin",
   ],
 }
