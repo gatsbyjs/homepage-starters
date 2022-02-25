622c811b94a90de1ce1dcd926aa022675a5bcd81
Alex Reed <alex.reed@gatsbyjs.com>
2022-02-25 13:44:25 -0500

    wip

--- plugins/datocms-plugin/scripts/import.js
+++ plugins/datocms-plugin/scripts/import.js
@@ -129,96 +129,106 @@ async function importContentModel(token) {
   return null
 }
 
-function couldBeID(val) {
-  return /^\d+$/.test(val) && val.length > 4
-}
+// function couldBeID(val) {
+//   return /^\d+$/.test(val) && val.length > 4
+// }
 
-const sortByRelation = (items, item, collection = []) => {
-  // console.log("items: ", items.length)
-  // console.log("item: ", item)
-  const index = items.findIndex((i) => i.id === item.id)
-  const nextItem = items[index + 1]
-  const sorted = collection.find((i) => i.id === item.id)
+// const sortByRelation = (items, item, collection = []) => {
+//   // console.log("items: ", items.length)
+//   // console.log("item: ", item)
+//   const index = items.findIndex((i) => i.id === item.id)
+//   const nextItem = items[index + 1]
+//   const sorted = collection.find((i) => i.id === item.id)
 
-  if (sorted) return collection
+//   if (sorted) return collection
 
-  Object.keys(item).forEach((key) => {
-    const val = item[key]
-    const relatedFields = []
+//   Object.keys(item).forEach((key) => {
+//     const val = item[key]
+//     const relatedFields = []
 
-    if (key !== "id" && couldBeID(val)) {
-      relatedFields.push(key)
-    }
+//     if (key !== "id" && couldBeID(val)) {
+//       relatedFields.push(key)
+//     }
 
-    if (relatedFields.includes(key)) {
-      const relation = items.find((i) => i.id === val)
-      if (relation) {
-        return sortByRelation(items, relation, collection)
-      }
-    }
+//     if (relatedFields.includes(key)) {
+//       const relation = items.find((i) => i.id === val)
+//       if (relation) {
+//         return sortByRelation(items, relation, collection)
+//       }
+//     }
 
-    if (Array.isArray(val)) {
-      val.forEach((relatedID) => {
-        const relation = items.find((i) => i.id === relatedID)
-        if (relation) {
-          return sortByRelation(items, relation, collection)
-        }
-      })
-    }
-  })
+//     if (Array.isArray(val)) {
+//       val.forEach((relatedID) => {
+//         const relation = items.find((i) => i.id === relatedID)
+//         if (relation) {
+//           return sortByRelation(items, relation, collection)
+//         }
+//       })
+//     }
+//   })
 
-  collection.push(item)
+//   collection.push(item)
 
-  if (nextItem) {
-    return sortByRelation(items, nextItem, collection)
-  }
+//   if (nextItem) {
+//     return sortByRelation(items, nextItem, collection)
+//   }
 
-  return collection
-}
+//   return collection
+// }
 
 async function importContent(token) {
   console.log(`Importing ${data.length} DatoCMS content records`)
   console.log("token: ", token)
-  const client = new SiteClient(token)
-
-  const existingRecords = await client.items.all({ allPages: true })
-  console.log("existing records: ", existingRecords)
+  // const client = new SiteClient(token)
 
   // const dataIds = data.map((point) => point.id)
   // console.log("data ids: ", dataIds)
   // console.log("first entry: ", data[0].id)
 
-  const orderedData = sortByRelation(data, data[0])
-  const errors = []
+  // get models from DatoCMS instance
+  const models = await DatoCmsTools.exportModels({ apiKey: token })
 
-  for (let i = 0; i < orderedData.length; i++) {
-    try {
-      const {
-        id,
-        meta,
-        updatedAt,
-        createdAt,
-        creator,
-        gatsbypreview,
-        ...recordData
-      } = orderedData[i]
-      console.log(`Creating record: ${id}`)
-      const record = await client.items.create(recordData)
-      console.log("created record: ", record)
-    } catch (e) {
-      console.error(`Error creating record: ${orderedData[i].id}`)
-      console.error(e)
-      errors.push(e)
-      break
-    }
-  }
+  console.log("models: ", models)
+
+  const result = await DatoCmsTools.importContent({
+    apiKey: token,
+    content: data,
+    models,
+  })
+
+  console.log("result: ", result)
+
+  // const orderedData = sortByRelation(data, data[0])
+  // const errors = []
+
+  // for (let i = 0; i < orderedData.length; i++) {
+  //   try {
+  //     const {
+  //       id,
+  //       meta,
+  //       updatedAt,
+  //       createdAt,
+  //       creator,
+  //       gatsbypreview,
+  //       ...recordData
+  //     } = orderedData[i]
+  //     console.log(`Creating record: ${id}`)
+  //     const record = await client.items.create(recordData)
+  //     console.log("created record: ", record)
+  //   } catch (e) {
+  //     console.error(`Error creating record: ${orderedData[i].id}`)
+  //     console.error(e)
+  //     errors.push(e)
+  //     break
+  //   }
+  // }
 
   console.log("Finished importing DatoCMS content records")
 
-  if (errors.length) {
-    fs.writeFileSync("datocms-errors.log", JSON.stringify(errors, null, 2))
-    return errors
-  }
+  // if (errors.length) {
+  //   fs.writeFileSync("datocms-errors.log", JSON.stringify(errors, null, 2))
+  //   return errors
+  // }
   return null
 }
 
