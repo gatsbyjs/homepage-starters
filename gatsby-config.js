09c40715bd33774967f0f706e1d74b1283d45397
jxnblk <jxnblk@gmail.com>
2022-03-29 11:43:09 -0400

    Fix WP image resolver

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   plugins: [
     // Comment and uncomment the desired CMS/backend plugin to switch between data sources
-    "contentful-plugin",
+    // "contentful-plugin",
     // "datocms-plugin",
     // "drupal-plugin",
-    // "wordpress-plugin",
+    "wordpress-plugin",
 
     // optional blog themes
     // "gatsby-theme-contentful-blog",
