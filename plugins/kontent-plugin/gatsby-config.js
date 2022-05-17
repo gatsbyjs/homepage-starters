c1069157bf89dd31eb8cd34746788f8f1c5e9647
Ivan Kiral <ivanK@kentico.com>
2022-05-17 10:11:30 +0200

    First try

---
+++ plugins/kontent-plugin/gatsby-config.js
@@ -0,0 +1,37 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://gatsbycontentfulhomepage.gatsbyjs.io/",
+    title: "Gatsby Contentful Homepage Starter",
+    author: `Gatsby`,
+    description: "A Gatsby Starter for building homepages with Contentful",
+  },
+  plugins: [
+    {
+      resolve: `@kentico/gatsby-source-kontent`,
+      options: {
+        projectId: process.env.KONTENT_PROJECT_ID,
+        languageCodenames: [`en-US`],
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-vanilla-extract",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Gatsby Starter Contentful Homepage",
+        short_name: "Gatsby",
+        start_url: "/",
+        background_color: "#ffe491",
+        theme_color: "#004ca3",
+        icon: "src/favicon.png",
+      },
+    },
+  ],
+}
