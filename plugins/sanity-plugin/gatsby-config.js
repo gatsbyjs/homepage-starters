0b7a37242e9791e6b127b11f4f2c162b4effbb06
Alex Reed <alex.reed@gatsbyjs.com>
2022-07-11 15:18:37 +0200

    update sanity env vars to match expected convention in cloud

--- plugins/sanity-plugin/gatsby-config.js
+++ plugins/sanity-plugin/gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
       resolve: "gatsby-source-sanity",
       options: {
         projectId: process.env.SANITY_PROJECT_ID,
-        dataset: process.env.SANITY_DATASET,
-        token: process.env.SANITY_TOKEN,
+        dataset: process.env.SANITY_PROJECT_DATASET,
+        token: process.env.SANITY_READ_TOKEN,
       },
     },
     "gatsby-plugin-sharp",
