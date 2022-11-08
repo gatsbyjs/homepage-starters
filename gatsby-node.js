8f26550e33c7f67ef1d5b734833f2dd50a7639ef
Alex Reed <alex.reed@gatsbyjs.com>
2022-11-08 00:15:21 -0500

    add tsx extension

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,11 +13,11 @@ exports.createPages = ({ actions }) => {
   const { createSlice } = actions
   createSlice({
     id: "header",
-    component: require.resolve("./src/components/header"),
+    component: require.resolve("./src/components/header.tsx"),
   })
   createSlice({
     id: "footer",
-    component: require.resolve("./src/components/footer"),
+    component: require.resolve("./src/components/footer.tsx"),
   })
 }
 
