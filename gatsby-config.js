a0876c343e901cfea17f203c2b772cd12b8c58cf
makoffee <makoffee@antidata.net>
2023-01-29 21:35:54 +0100

    trying stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     // Comment and uncomment the desired CMS/backend plugin to switch between data sources
-    "contentful-plugin",
+    // "contentful-plugin",
     // "datocms-plugin",
     // "drupal-plugin",
     // "wordpress-plugin",
@@ -9,7 +9,7 @@ module.exports = {
     // "kontent-ai-plugin",
 
     // optional blog themes
-    // "gatsby-theme-contentful-blog",
+    "gatsby-theme-contentful-blog",
     // "gatsby-theme-datocms-blog",
     // "gatsby-theme-wordpress-blog",
   ],
